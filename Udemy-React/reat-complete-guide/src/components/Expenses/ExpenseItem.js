3e34678e0316f131846d1dea0dc693d3812e6ca0
Rushil Borad <rushilborad@gmail.com>
2023-02-09 15:30:46 +0530

    removed unused components

--- Udemy-React/reat-complete-guide/src/components/Expenses/ExpenseItem.js
+++ Udemy-React/reat-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,29 +1,18 @@
-import {useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 
 
 const ExpenseItem= (props) =>  {
-  
-
   // let title= props.title;
-  const [title, setTitle] = useState(props.title);
-  
-  function clickHandler() {
-    setTitle('Updated!');
-    console.log(title);
-  }
-
-
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+    
     </Card>
   );
 }
